acbeccd0181d9f3eabac4e5748ca9038b6a63890
Ulises Colosimo <colosimo101@gmail.com>
2025-09-22 14:12:37 -0300

    first commit dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'educgi';
+  title = 'CGI-CLINICS';
+
+  onLoginSubmit(event: Event) {
+    event.preventDefault();
+    // TODO: Implement login functionality
+    console.log('Login form submitted');
+    alert('Login functionality will be implemented here.');
+  }
+
+  showGrantInfo() {
+    // TODO: Show grant information in a tooltip or modal
+    alert('Grant Agreement CGI-Clinics 101057509\n\nThis project has received funding from the European Union\'s Horizon Europe programme.');
+  }
 }
